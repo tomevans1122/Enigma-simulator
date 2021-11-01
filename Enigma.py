5a7fb3e028d70e5d016b7bf4ad946740a5283e16
Tom <66395708+tomevans1122@users.noreply.github.com>
2021-11-01 12:46:10 +0000

    Update Enigma.py

--- Enigma.py
+++ Enigma.py
@@ -1,4 +1,5 @@
 import collections
+import math
 
 # Letters to numbers and numbers to letters dictionaries
 LTN_alphabet = {
@@ -84,22 +85,25 @@ number_lst = [0,
               24,
               25]
 
-# Created a pseudo-random ordering for rotors using random.shuffle(number_lst) and assigning numeral
-I = [8, 21, 17, 11, 3, 15, 20, 22, 18, 14, 12, 10, 4, 23, 5, 1, 13, 25, 0, 6, 19, 24, 2, 9, 7, 16]
-II = [21, 16, 3, 24, 10, 6, 23, 5, 15, 20, 22, 18, 7, 0, 12, 8, 1, 9, 4, 13, 2, 17, 25, 19, 11, 14]
-III = [5, 15, 21, 24, 14, 25, 16, 11, 12, 22, 7, 20, 18, 8, 2, 23, 1, 17, 10, 4, 9, 3, 6, 0, 13, 19]
-IV = [8, 20, 21, 10, 9, 5, 0, 11, 2, 12, 3, 25, 18, 6, 4, 16, 19, 22, 15, 1, 7, 23, 13, 24, 14, 17]
-V = [23, 22, 12, 10, 1, 11, 17, 6, 0, 21, 19, 13, 8, 3, 4, 5, 9, 7, 2, 25, 16, 15, 14, 24, 20, 18]
+# Original rotor values for Wehrmacht enigma machines
+I = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
+II = [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
+III = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
+IV = [4, 18, 14, 21, 15, 25, 9, 0, 24, 16, 20, 8, 17, 7, 23, 11, 13, 5, 19, 6, 10, 3, 2, 12, 22, 1]
+V = [21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10]
+
+#reflector ordering before the signal passes back through rotors and plugboard again
+Reflector = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
 
 # turnover positions of each rotor in order
 turnover_positions = [17, 5, 22]
 
 
 # In reality, these are user inputted and changed daily. For simulation arbitrary choice will suffice.
-test_plugboard_vals = [1, 2, 3, 11, 17, 15, 7, 8, 18, 14, 12, 10, 4, 23, 5, 22, 13, 25, 16, 6, 19, 24, 21, 9, 20, 0]
+test_plugboard_vals = [0, 24, 2, 3, 4, 5, 22, 7, 8, 9, 10, 12, 11, 16, 14, 15, 13, 17, 18, 19, 20, 21, 6, 23, 1, 25]
 test_word = 'TURING'
 rotor_choice = [I, II, III]     # choice of 5 rotors in any order
-rotor_positions = [5, 2, 12]    # initial rotor settings for each rotor (i.e. letter pairings)
+rotor_positions = [7, 24, 0]    # initial rotor settings for each rotor (i.e. letter pairings)
 
 
 # Convert inputted string into a list
@@ -127,36 +131,64 @@ aligned_rotor1 = shuffle_to_right(rotor_choice[0], rotor_positions[0])
 aligned_rotor2 = shuffle_to_right(rotor_choice[1], rotor_positions[1])
 aligned_rotor3 = shuffle_to_right(rotor_choice[2], rotor_positions[2])
 
-i = 0
-encrypted_lst = []
-while i < len(numbered_input):
-    for num in numbered_input:
-        plug1 = test_plugboard_vals[num]
-        shuffled_rotor = shuffle_to_right(aligned_rotor1, i)
-        rotor1 = shuffled_rotor[plug1]
-                if rotor1 == turnover_positions[0]:
-            shifted_second_rotor = shuffle_to_right(aligned_rotor2, 1)
-            turnover_rotor2 = shifted_second_rotor[rotor1]
-            rotor3 = aligned_rotor3[turnover_rotor2]
-            # now goes through the machine in reverse order
-            reflected_rotor3 = aligned_rotor3[rotor3]
-            reflected_rotor2 = turnover_rotor2[reflected_rotor3]
-            reflected_rotor1 = shuffled_rotor[reflected_rotor2]
-            reflected_plug = test_plugboard_vals[reflected_rotor1]
-            encrypted_lst.append(reflected_plug)
-            i += 1
-            continue
-        else:
-            rotor2 = aligned_rotor2[rotor1]
-            rotor3 = aligned_rotor3[rotor2]
-            # now goes through the machine in reverse order
-            reflected_rotor3 = aligned_rotor3[rotor3]
-            reflected_rotor2 = aligned_rotor2[reflected_rotor3]
-            reflected_rotor1 = shuffled_rotor[reflected_rotor2]
-            reflected_plug = test_plugboard_vals[reflected_rotor1]
-            encrypted_lst.append(reflected_plug)
-            i += 1
-            continue
+#ENIGMA PROCESS
+# passing word through plugboard
+def plugboard_or_reflector(plugboard_vals, numbered_word):
+    plug_vals = []
+    for i in numbered_word:
+        plug_vals.append(plugboard_vals[i])
+    return plug_vals
+
+# shifting rotor each turn
+def shift_rotor(aligned_rotor, n, rotor_num):
+    if rotor_num == 1:
+        n = -n % len(aligned_rotor)
+
+    elif rotor_num == 2:
+        n = - math.trunc(n / 25) % len(aligned_rotor)
+
+    elif rotor_num == 3:
+        n = - math.trunc(n / 675) % len(aligned_rotor)
+
+    return shuffle_to_right(aligned_rotor, n)
+
+# passing letters through the rotor (using shifting function)
+def through_rotor(aligned_rotor, previous_sequence, rotor_num):
+    number = []
+    for i in range(len(previous_sequence)):
+        rotation = shift_rotor(aligned_rotor, i, rotor_num)
+        number.append(rotation[previous_sequence[i]])
+    return number
+
+
+def rotor_reverse(seq):
+    rotor_rev = [0] * 26
+    for i in range(26):
+        rotor_rev[seq[i]] = i
+    return rotor_rev
+
+
+def through_rotor_reversed(aligned_rotor, previous_sequence, rotor_num):
+    reversed_vals = []
+    for i in range(len(previous_sequence)):
+        rotation = shift_rotor(aligned_rotor, i, rotor_num)
+        sequence_reverse = rotor_reverse(rotation)
+        reversed_vals.append(sequence_reverse[previous_sequence[i]])
+    return reversed_vals
+
+
+# EXECUTION
+SEQ1 = plugboard_or_reflector(test_plugboard_vals, numbered_input)
+SEQ2 = through_rotor(aligned_rotor1, SEQ1, 1)
+SEQ3 = through_rotor(aligned_rotor2, SEQ2, 2)
+SEQ4 = through_rotor(aligned_rotor3, SEQ3, 3)
+SEQ5 = plugboard_or_reflector(Reflector, SEQ4)
+SEQ6 = through_rotor_reversed(aligned_rotor3, SEQ5, 3)
+SEQ7 = through_rotor_reversed(aligned_rotor2, SEQ6, 2)
+SEQ8 = through_rotor_reversed(aligned_rotor1, SEQ7, 1)
+SEQ9 = plugboard_or_reflector(test_plugboard_vals, SEQ8)
+
+
 
 # # Change numbers back into letters
 encrypted_letters = []
