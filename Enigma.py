6ac4894dd59b358de79309e7b0da9c3f6d4fdb56
Tom <66395708+tomevans1122@users.noreply.github.com>
2023-09-23 11:36:08 +0100

    Update Enigma.py

--- Enigma.py
+++ Enigma.py
@@ -85,7 +85,7 @@ number_lst = [0,
               24,
               25]
 
-# Original rotor values for Wehrmacht enigma machines
+# Original rotor values for German army enigma machines
 I = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
 II = [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
 III = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
